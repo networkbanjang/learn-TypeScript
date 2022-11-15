b34d9d532323133877a74dadca5f04c68dec40a9
networkbanjang <tmddud73@gmail.com>
2022-11-15 18:34:40 +0900

    Build

--- Clone_Project/bulid/index.js
+++ Clone_Project/bulid/index.js
@@ -1,9 +1,40 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const crypto_1 = __importDefault(require("crypto"));
 class Block {
-    constructor(data) {
+    constructor(prevHash, height, data) {
+        this.prevHash = prevHash;
+        this.height = height;
         this.data = data;
+        this.hash = Block.callculateHash(prevHash, height, data);
     }
-    static hello() {
-        return 'hi';
+    static callculateHash(prevHash, height, data) {
+        const toHash = `${prevHash}${height}${data}`;
+        return crypto_1.default.createHash("sha256").update(toHash).digest("hex");
     }
 }
+class BlcokChain {
+    constructor() {
+        this.blocks = [];
+    }
+    getPrevhash() {
+        if (this.blocks.length === 0)
+            return "";
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+    addBlock(data) {
+        const newBlock = new Block(this.getPrevhash(), this.blocks.length + 1, data);
+        this.blocks.push(newBlock);
+    }
+    getBlocks() {
+        return [...this.blocks];
+    }
+}
+const blockChain = new BlcokChain();
+blockChain.addBlock('first one');
+blockChain.addBlock('second one');
+blockChain.addBlock('third one');
+console.log(blockChain.getBlocks());
---
+++ Clone_Project/bulid/jsDoc.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const myJs_1 = require("./myJs");
+(0, myJs_1.init)({
+    url: "string",
+});
+(0, myJs_1.exit)(13);
